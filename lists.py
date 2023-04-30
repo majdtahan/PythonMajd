d1bece5afc709c08e6a733a2a76b645a8eba272d
Majd El-Tahan <majdtahan@hotmail.com>
2023-04-30 09:45:19 -0400

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,19 @@
+my_list = [1,2,3]
+my_list2 = ['String', 100, 40.3]
+another_list = [4,5]
+new_list = my_list + another_list
+new_list.append('six')
+new_list[0] = 'ONE ALL CAPS'
+new_list.append('seven')
+popped_item = new_list.pop(0)
+
+
+
+
+
+
+
+
+
+print(new_list)
+print(popped_item)
